530dc74c3cb2f06b5675101ddf9c7d0cf8b4832a
HydeZero <zac10smith18@gmail.com>
2023-08-09 15:06:06 -0500

    Initial commit

---
+++ Filemngr.cs
@@ -0,0 +1,120 @@
+﻿using Cosmos.System.FileSystem;
+using MIV;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Schema;
+using Sys = Cosmos.System;
+
+namespace AetherOS
+{
+    internal class Filemngr
+    {
+        public static string file;
+        public void RunFileMNGR()
+        {
+            Console.Clear();
+            Sys.FileSystem.CosmosVFS FS = new();
+            while (true)
+            {
+                Console.WriteLine("Welcome to File Manager. Type 'help' for a list of commands.");
+                var input = Console.ReadLine();
+                if (input == "about")
+                {
+                    Console.WriteLine("File Manager Version 1.0 FOR AetherOS.\nKnown Limitations:\n  Basically only useful for reading and writing .txt files.");
+                }
+                else if (input == "createdir")
+                {
+                    Console.WriteLine("What would you like to create? Here are your choices:\n  folder\n  file\nType the object you want to create.");
+                    input = Console.ReadLine();
+                    if (input == "folder")
+                    {
+                        Console.WriteLine("name the file path. Don't worry, the drive number will be added for you.");
+                        input = Console.ReadLine();
+                        try
+                        {
+                            FS.CreateDirectory(@$"0:\{input}");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.ToString());
+                        }
+                    }
+                    else if (input == "file")
+                    {
+                        Console.WriteLine("name the file path AND the file extension. Don't worry, the drive number will be added for you.");
+                        try
+                        {
+                            File.Create($@"0:\{Console.ReadLine()}");
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine(e.ToString());
+                            break;
+                        }
+                        Console.WriteLine($"File created at directory 0:\\{input}. Use 'modifyfile' to open the editor");
+                    }
+                }
+                else if (input == "exit")
+                {
+                    Console.WriteLine("Returning to AetherOS...");
+                    break;
+                }
+                else if (input == "help")
+                {
+                    Console.WriteLine("Commands for File Manager:\nabout: shows info about this software.\ncreate: creates either a folder or text file.\nlistfiles:lists all the files in a directory(this may take some time, depending on the amount of files you have)\nlistdir: lists all the directories\ncreate: creates a folder or file\nreadfile: reads a file in the directory you provide.\ndeletefile: deletes a file\ndeletedir: deletes a directory.\nexit: exits the file manager.");
+                }
+                else if (input == "listfiles")
+                {
+                    Console.WriteLine("choose the directory to list files from.");
+                    var file_list = Directory.GetFiles($@"0:\{Console.ReadLine()}");
+                    foreach (var file in file_list)
+                    {
+                        Console.WriteLine(file);
+                    }
+                }
+                else if (input == "listdir")
+                {
+                    Console.WriteLine("choose the directory to list subdirectories from. leave blank to examine the root.");
+                    var directory_list = Directory.GetDirectories($@"0:\{Console.ReadLine()}");
+                    foreach (var directory in directory_list)
+                    {
+                        Console.WriteLine(directory);
+                    }
+                }
+                else if (input == "deletefile")
+                {
+                    Console.WriteLine("Type in EXACTLY the file you want to delete. If you make a typo, the machine MAY crash.");
+                    File.Delete(@$"0:\{Console.ReadLine()}");
+                    Console.WriteLine("Deleted.");
+                }
+                else if (input == "deletedir")
+                {
+                    Console.Write("Type in the directory you want to erase. The directory MUST be empty.");
+                    string directoryToErase = Console.ReadLine();
+                    directoryToErase = $@"0:\{directoryToErase}";
+                    Console.WriteLine($"Deleting {directoryToErase}.");
+                    try
+                    {
+                        Directory.Delete(directoryToErase);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+                }
+                else if (input == "modifyfile")
+                {
+                    Console.WriteLine("You will be booted into MIV. Press y to continue.");
+                    if (Console.ReadLine().ToLower() == "y")
+                    {
+                        Console.Clear();
+                        MIV.MIV.StartMIV();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Kernel.cs
@@ -0,0 +1,111 @@
+﻿using Cosmos.System.FileSystem;
+using Cosmos.System.Graphics;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Xml.Schema;
+using Sys = Cosmos.System;
+
+namespace AetherOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        Sys.FileSystem.CosmosVFS FS = new();
+
+        Filemngr filemngr = new();
+
+        public const string Version = "0.9 Dev";
+
+        public const string userDataPath = @"0:\systemdata\userdata.txt";
+
+        public string nameOfUser;
+
+        protected override void BeforeRun()
+        {
+            // Does system file checks
+            Console.WriteLine("Initializing File System...");
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(FS);
+            Console.WriteLine("Checking systemdata folder...");
+            if (FS.GetDirectory(@"0:\systemdata") == null)
+            {
+                Console.WriteLine("No systemdata folder found. Creating one.");
+                FS.CreateDirectory(@"0:\systemdata");
+            }
+            Console.WriteLine("All checks good. AetherOS is now booting up...");
+        }
+
+        protected override void Run()
+        {
+            if (FS.GetFile(userDataPath) == null)
+            {
+                Console.WriteLine("No user file detected. Customize yourself a user? [y/n]");
+                var choice = Console.ReadLine();
+                if (choice == "y")
+                {
+                    Console.WriteLine("What is your name?");
+                    var name = Console.ReadLine();
+                    FS.CreateFile(userDataPath);
+                    File.WriteAllText(userDataPath, name);
+                    nameOfUser = name;
+                }
+                else
+                {
+                    FS.CreateFile(userDataPath);
+                    File.WriteAllText(userDataPath, "user");
+                    nameOfUser = "user";
+                }
+            }
+            else
+            {
+                nameOfUser = File.ReadAllText(userDataPath);
+            }
+            Console.Write($"What would you like to do, {nameOfUser}? (type 'help' for list of commands): ");
+            var input = Console.ReadLine();
+            if (input == "help")
+            {
+                Console.WriteLine("Help:");
+                Console.WriteLine("filemngr: boots into file manager, where you can manage files.\nabout: shows system info\nexit: shuts down or restarts the machine.\nuser: modifies the user's name on file.");
+            }
+            else if (input == "about")
+            {
+                Console.WriteLine($"About AetherOS: \n Version: {Version} \n User: {nameOfUser} \n Library Used: COSMOS Userkit 20221121 for VS 2022 \n Other Software Used: \n    MIV by Github user bartashevich");
+            }
+            else if (input == "user")
+            {
+                Console.WriteLine("Choose new name for user.");
+                nameOfUser = Console.ReadLine();
+                File.WriteAllText(userDataPath, nameOfUser);
+            }
+            else if (input == "filemngr")
+            {
+                Console.WriteLine("Booting into File Manager...");
+                filemngr.RunFileMNGR();
+            }
+            else if (input == "exit")
+            {
+                Console.WriteLine("Choose a power option. 'cancel' to cancel. Your 2 options are:\n  shutdown\n  reboot");
+                input = Console.ReadLine();
+                if (input == "shutdown")
+                {
+                    Shutdown();
+                }
+                if (input == "reboot")
+                {
+                    Reboot();
+                }
+            }
+        }
+
+        public static void Shutdown()
+        {
+            Sys.Power.Shutdown();
+        }
+
+        public static void Reboot()
+        {
+            Sys.Power.Reboot();
+        }
+
+    }
+}
